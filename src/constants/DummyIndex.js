4bfb227176307fcb7f90afc2c6630dbebd07d7b1
unawarexi <unawarexi@gmail.com>
2024-03-24 15:10:25 +0100

    dummy

---
+++ src/constants/DummyIndex.js
@@ -0,0 +1,42 @@
+export const categoryData = [
+    {
+        name : "Carbs",
+        image: "https://www.themealdb.com/images/media/meals/kos9av1699014767.jpg"
+    },
+    {
+        name : "Dessert",
+        image: "https://www.themealdb.com/images/media/meals/oe8rg51699014028.jpg"
+    },
+    {
+        name : "Proteins",
+        image: "https://www.themealdb.com/images/ingredients/Beef.png"
+    }
+]
+
+
+export const mealData = [
+    {
+        name: "Potato Salad (Olivier Salad)",
+        image: "https://www.themealdb.com/images/media/meals/ebvuir1699013665.jpg"
+    },
+    {
+        name: "Bread omelette",
+        image: "https://www.themealdb.com/images/media/meals/hqaejl1695738653.jpg"
+    },
+    {
+        name: "Fish Soup (Ukha)",
+        image: "https://www.themealdb.com/images/media/meals/7n8su21699013057.jpg"
+    },
+    {
+        name: "Beetroot Soup (Borscht)",
+        image: "https://www.themealdb.com/images/media/meals/zadvgb1699012544.jpg"
+    },
+    {
+        name: "Blini Pancakes",
+        image: "https://www.themealdb.com/images/media/meals/0206h11699013358.jpg"
+    },
+    {
+        name: "Kentucky Fried Chicken",
+        image: "https://www.themealdb.com/images/media/meals/xqusqy1487348868.jpg"
+    },
+]
