52fdee913f88282d060b24f5e62f428a94213621
unawarexi <unawarexi@gmail.com>
2024-03-24 12:44:38 +0100

    UseEffect && navigation to mount

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
-import { Image, Text, View } from "react-native";
 import React, { useEffect } from "react";
+import { Image, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Logo } from "../../assets/Image";
 
@@ -8,23 +8,29 @@ import {
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-import Animated, {
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 const WelcomeScreen = () => {
   const ring1padding = useSharedValue(0);
   const ring2padding = useSharedValue(0);
 
   useEffect(() => {
+    ring1padding.value = 0;
+    ring2padding.value = 0;
     setTimeout(() => {
       ring1padding.value = withSpring(ring1padding.value + hp(5));
     }, 100);
     setTimeout(() => {
-      ring2padding.value = withSpring(ring2padding.value + hp(5));
+      ring2padding.value = withSpring(ring2padding.value + hp(5.5));
     }, 300);
+
+    setTimeout(() => {
+      Navigation.navigate("Home");
+    }, 2500);
   }, []);
+
+  const Navigation = useNavigation();
 
   return (
     <View className="flex-1 items-center justify-center space-y-10 bg-amber-500">
@@ -32,11 +38,11 @@ const WelcomeScreen = () => {
 
       {/* {LOGO IMAGE WITH RINGS} */}
       <Animated.View
-        className="bg-white/20 rounded-ful "
+        className="bg-white/20 rounded-full"
         style={{ padding: ring1padding }}
       >
         <Animated.View
-          className=" bg-white/20 rounded-full "
+          className=" bg-white/20 rounded-full"
           style={{ padding: ring2padding }}
         >
           <Image source={Logo} style={{ width: hp(20), height: hp(20) }} />
