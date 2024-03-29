e38ef732c8117fee87c710c8da03c70e0029f14b
unawarexi <unawarexi@gmail.com>
2024-03-29 14:12:42 +0100

    Fixed

--- src/helpers/CacheImage.js
+++ src/helpers/CacheImage.js
@@ -2,47 +2,68 @@ import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Animated from "react-native-reanimated";
 
-const CacheImage = ({ uri }) => {
-  const [cachedUri, setCachedUri] = useState(null);
 
-  useEffect(() => {
-    const fetchCachedUri = async () => {
-      try {
-        // Check if image URI is already cached
-        const cachedImageUri = await AsyncStorage.getItem(uri);
-        if (cachedImageUri) {
-          setCachedUri(cachedImageUri);
-        } else {
-          // Image is not cached, download and cache it
-          const response = await fetch(uri);
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const blob = await response.blob();
-          const dataUrl = await readBlobAsDataURL(blob);
-          setCachedUri(dataUrl);
-          await AsyncStorage.setItem(uri, dataUrl);
+const MAX_CACHE_SIZE = 1024 * 1024 * 10; // 10 MB
+
+// uppercase
+const CacheImage = ({ uri, ...props }) => {
+    const [cachedUri, setCachedUri] = useState(null);
+
+    useEffect(() => {
+        const getCachedImage = async () => {
+            try {
+                 // Check if image URI is already cached
+                const cachedImageData = await AsyncStorage.getItem(uri);
+
+                if (cachedImageData) {
+                    setCachedUri({ uri: cachedImageData });
+                } else {
+
+                    // Image is not cached, download and cache it
+                    const response = await fetch(uri);
+                    const imageBlob = await response.blob();
+                    const base64Data = await new Promise((resolve, reject) => {
+                        // read content of file
+                        const reader = new FileReader();
+                        reader.readAsDataURL(imageBlob);
+                        reader.onloadend = () => {
+                            resolve(reader.result);
+                        };
+                        reader.onerror = reject;
+                    });
+
+                    await cacheImage(uri, base64Data);
+                    setCachedUri({ uri: base64Data });
+                }
+
+            } catch (error) {
+                console.error("Error fetching or caching image:", error);
+            }
+        };
+
+        getCachedImage();
+    }, [uri]); // Re-run effect only if URI changes
+
+    // lowercase
+    const cacheImage = async (uri, data) => {
+        try {
+            await AsyncStorage.setItem(uri, data);
+
+            // -------------- Check cache size
+            const cacheSize = await AsyncStorage.getSize();
+
+            if (cacheSize > MAX_CACHE_SIZE) {
+                //------------------  Implement cache eviction strategy (e.g., remove oldest items)
+                const allKeys = await AsyncStorage.getAllKeys();
+                const oldestKey = allKeys[0]; // -------------- For simplicity, just remove the first key
+                await AsyncStorage.removeItem(oldestKey);
+            }
+        } catch (error) {
+            console.error("Error caching image:", error);
         }
-      } catch (error) {
-        console.log("Error fetching or caching image:", error);
-      }
     };
 
-    fetchCachedUri();
-  }, [uri]); // Re-run effect only if URI changes
-
-  const readBlobAsDataURL = (blob) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        resolve(reader.result);
-      };
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  };
-
-  return cachedUri ? <Animated.Image source={{ uri: cachedUri }} /> : null;
+    return <Animated.Image source={cachedUri} {...props} />;
 };
 
 export default CacheImage;
